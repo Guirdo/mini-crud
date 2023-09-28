5776097c85c285f580d53620d9ae4532975467a9
Guirdo <produccionesaldo@gmail.com>
2023-09-28 12:33:10 -0600

    Delete company

--- src/components/Companies/Companies.jsx
+++ src/components/Companies/Companies.jsx
@@ -1,10 +1,13 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-import { Button, Table } from "reactstrap"
+import { Link, useNavigate } from "react-router-dom"
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader, Table } from "reactstrap"
 
 export default function Companies() {
+  const navigate = useNavigate()
   const [companies, setCompanies] = useState([])
+  const [modal, setModal] = useState(false)
+  const [aboutToDeleteCompany, setAboutToDeleteCompany] = useState({})
 
   useEffect(() => {
     axios.get('https://mini-crud-api.vercel.app/api/v1/companies')
@@ -12,6 +15,19 @@ export default function Companies() {
         setCompanies(res.data.companies)
       })
   })
+
+  const openModal = (company) => {
+    setModal(true)
+    setAboutToDeleteCompany(company)
+  }
+
+  const handleDeleteCompany = (id) => {
+    axios.delete(`https://mini-crud-api.vercel.app/api/v1/companies/${id}`)
+      .then(res => {
+        navigate('/companies')
+      })
+    setModal(false)
+  }
 
   return (
     <>
@@ -37,13 +53,30 @@ export default function Companies() {
                   >
                     Editar
                   </Link>
-                  <Button>Eliminar</Button>
+                  <Button onClick={()=>openModal(company)}>Eliminar</Button>
                 </td>
               </tr>
             ))
           }
         </tbody>
       </Table>
+
+      {/* Modal para eliminar a una empresa */}
+
+      <Modal isOpen={modal} toggle={()=>setModal(!modal)}>
+        <ModalHeader toggle={()=>setModal(!modal)}>Modal title</ModalHeader>
+        <ModalBody>
+          ¿Estas seguro de eliminar la empresa <span className="fw-bold"> {aboutToDeleteCompany.name}</span>?
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={()=> handleDeleteCompany(aboutToDeleteCompany._id)}>
+            Eliminar
+          </Button>{' '}
+          <Button color="secondary" onClick={()=>setModal(false)}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
     </>
   )
 }
